9137ce6f03f22d77da0426a93960df842968ad21
gtbu <60353863+g7sim@users.noreply.github.com>
2023-06-10 14:44:52 +0200

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,8 +1,12 @@
 CKEDITOR.plugins.add('ckeditorfa6',{icons:'ckeditorfa',
+
+onLoad: function() {
+CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditorfa6.js');
+CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditorfa6.css');
+},
+
 init:function(editor){
 editor.addCommand('ckeditorfa6', new CKEDITOR.dialogCommand('ckeditorFaDialog',{allowedContent:'span(!fa-*)'}));
 editor.ui.addButton('ckeditorfa6',{label:'FontAwesome6 icons',command:'ckeditorfa6',toolbar:'insert',icon:this.path + 'icons/ckeditorfa.png'});
-CKEDITOR.dialog.add('ckeditorFaDialog', this.path + 'dialogs/ckeditorfa6.js');
-CKEDITOR.document.appendStyleSheet(this.path + 'css/ckeditorfa6.css');
 }
 });
